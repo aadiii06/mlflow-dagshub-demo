587e3b7cd755b7befd7e4ab4a264b8a6a515fa4a
Aadish Jain <ajaadish06@gmail.com>
2025-11-20 20:53:56 +0530

    initial commit

---
+++ iris_db.py
@@ -0,0 +1,52 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# load dataset
+data = load_iris()
+X = data.data
+y = data.target
+
+# split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define parameters 
+max_depth = 10
+
+# apply mlfow
+mlflow.set_experiment("iris_db")
+with mlflow.start_run():
+    
+    rf = DecisionTreeClassifier(max_depth=max_depth)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_param("max_depth", max_depth)
+    
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=data.target_names, yticklabels=data.target_names)
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    
+    #Save the plot as an artifact
+    plt.savefig("confusion_matrix.png")
+    
+    # mlflow code
+    mlflow.log_artifact("confusion_matrix.png")
+    
+    mlflow.log_artifact(__file__)
+    
+    mlflow.sklearn.log_model(rf, "decision_tree_model")
+    
+    print(f"Model accuracy: {accuracy}")
